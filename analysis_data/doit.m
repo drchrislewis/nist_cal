7dd7754e77504d8f8d6b671773bc6c881e27ce7a
clewis <clewis@swri.org>
2015-02-27 13:49:00 -0600

    added stuff for post analysis

---
+++ analysis_data/doit.m
@@ -0,0 +1,26 @@
+field_results;
+% compute magnitude of errors for display
+frows = 2/0.1+1;
+fcols = 2/0.1+1;
+z = [];
+zmax=1.0;
+for i=1:fcols, for j=1:frows,fi=(i-1)*frows+j; ex=f(fi,7);ey=f(fi,8);ez=f(fi,9);z(j,i) = sqrt(ex*ex+ey*ey+ez*ez); end; end;
+for i=1:fcols, for j=1:frows,fi=(i-1)*frows+j; if (z(j,i)>zmax), z(j,i) = zmax; endif; end; end;
+y = -1:0.1:1.0;
+x = -1:0.1:1.0;
+figure(1);
+mesh(x,y,z);
+xlabel('Meters');
+ylabel('Meters');
+zlabel('Meters');
+title('Localization Accuracy for work volume (Meters)')
+print -djpg heatmesh.jpg
+
+figure(2);
+contourf(x,y,z);
+colorbar();
+xlabel('Meters');
+ylabel('Meters');
+title('Localization Accuracy in Inches for work Volume')
+print -djpg heatimage.jpg
+
